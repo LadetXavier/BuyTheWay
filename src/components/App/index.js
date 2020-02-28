945547a4f1768ee0b9409d9c045860eae780fa04
LadetXavier <xavier.ladet@yahoo.fr>
2020-02-28 12:04:58 +0100

    First Commit Clone model + Redux

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+// == Composant
+const App = () => (
+  <div className="app">
+    <h1>Composant : App</h1>
+  </div>
+);
+
+// == Export
+export default App;
