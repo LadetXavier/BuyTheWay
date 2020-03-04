f2322f4736f2ebd1b6a972a8cec6b516d0e633d5
Tristan LE PARE <tristan.lepare@hotmail.fr>
2020-03-04 17:20:53 +0100

    Intégration Header/Navbar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 
-import Route from './Routes.js';
-
+import Route from './Routes';
+import 'src/assets/styles/reset.css';
+import './styles.scss';
 
 
 // == Composant
-const App = () => {
-  return(    
-  <Route/>  
+const App = () => (
+  <Route />
 );
-}
+
 
 // == Export
 export default App;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,27 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 import './styles.scss';
+import '../../assets/FontAwesome/css/all.css';
+import '../../assets/fonts/Roboto-Regular.ttf';
+import Logo from './logo';
 
 const Header = () => (
-
-  <div className="header">
-    <h1>header</h1>
-  </div>
-  
+  <nav className="navbar">
+    <a href="/"> <Logo /> </a>
+    <ul className="nav-links">
+      <li><a id="men" className="categories" href="/">HOMMES</a></li>
+      <li><a id="women" className="categories" href="/">FEMMES</a></li>
+      <li><a id="trend" className="categories" href="/">TENDANCES</a></li>
+      <li><a id="challenges" className="categories" href="/">DEFIS</a></li>
+    </ul>
+    <ul className="nav-icons">
+      <li><a className="far fa-user icons" /></li>
+      <li><a className="far fa-shopping-cart icons" /></li>
+      <li><a className="far fa-search icons" /></li>
+    </ul>
+  </nav>
 );
 
 export default Header;
