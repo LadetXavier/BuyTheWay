28bffc8e1c7283b88eccffab2dd008b5187e07e2
Tristan LE PARE <tristan.lepare@hotmail.fr>
2020-03-09 14:52:34 +0100

    présentation

--- src/pages/category.js
+++ src/pages/category.js
@@ -1,11 +1,13 @@
-import React, {useEffect} from 'react'
+/* eslint-disable import/prefer-default-export */
+import React, { useEffect } from 'react';
 import { saveProducts } from 'src/actions/shop.js';
 import { apiError } from 'src/actions/apiActions.js';
 import Loader from 'src/components/Loader.js';
 import { Link } from 'react-router-dom';
 import Hierarchy from 'src/components/shop/Hierarchy.js';
-import ItemCard from 'src/components/shop/ItemCard.js';
-
+//import ItemCard from 'src/components/shop/ItemCard.js';
+import ItemCard from 'src/components/ItemCard';
+import { data } from 'src/components/ItemCard/data';
 import PropTypes from 'prop-types';
 import './category.scss';
 
@@ -19,41 +21,41 @@ export const Category = ({
   useEffect(() => {
     requestAction({
       url: `http://54.164.43.47:3000/category/${match.params.category}`,
-      onSucess: saveProducts,
+      onSuccess: saveProducts,
       onFailure: apiError,
       label: 'productsLoading',
     });
   }, []);
 
-   // Display the loading icon by default
-   let displayed = <Loader />;
-  
-   // Once datas are collected, display the dynamique content
-   if (!isLoading) {
-     if(hasError) {
-       displayed = <p>Un problème est survenu, il semblerait que notre serveur soit momentanément innacessible, veuillez réessayer plus tard.</p>
-     }
-     else {
-      const categoryName = listProducts[0].category.type;
-     let productArray = listProducts.map((itemCurrent) => (
-      <ItemCard key={itemCurrent._id} item={itemCurrent} />
-    )); 
+  // Display the loading icon by default
+  let displayed = <Loader />;
 
-     displayed = 
-       (<>
+  // Once datas are collected, display the dynamic content
+  if (!isLoading) {
+    if (hasError) {
+      //displayed = <p>Un problème est survenu, il semblerait que notre serveur soit momentanément innacessible, veuillez réessayer plus tard.</p>;
+      //console.log(data.category[0].picture.picture1);
+      displayed = (<ItemCard picture={data.category[0].picture.picture1} price={data.category[0].price} name={data.category[0].price} />);
+    }
+    /* else {
+      const categoryName = listProducts[0].category.type;
+      let productArray = listProducts.map((itemCurrent) => (
+        <ItemCard key={itemCurrent._id} item={itemCurrent} />
+      )); 
+
+      displayed = (
+        <>
           <Hierarchy match={match} categoryName={categoryName}/>
           <section className="list-products">
-           {productArray}
+            {productArray}
           </section> 
-     </>);
-    }       
+        </>);
+    } */
   }
 
   return (
     <div>
       {displayed}
     </div>
-  )
-}
-
-
+  );
+};
