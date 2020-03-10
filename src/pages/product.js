e248c7a427e86cd6f174bde1c8c38c0a46bf0556
Tristan LE PARE <tristan.lepare@hotmail.fr>
2020-03-10 14:04:50 +0100

    handle change picture

--- src/pages/product.js
+++ src/pages/product.js
@@ -5,7 +5,7 @@ import Loader from 'src/components/Loader.js';
 import { Link } from 'react-router-dom';
 import Hierarchy from 'src/components/shop/Hierarchy';
 import PropTypes from 'prop-types';
-import {FormPurchaseContainer as FormPurchase} from 'src/container/components/FormPurchase.js';
+import { FormPurchaseContainer as FormPurchase } from 'src/container/components/FormPurchase.js';
 
 import './product.scss';
 
@@ -16,20 +16,30 @@ export const Product = ({
   match,
   productDetail,
 }) => {
+  const mainPic = React.createRef();
+  const secondPic = React.createRef();
+
+  const handleChangePic = () => {
+    mainPic.current.classList.toggle('product-mainImg');
+    mainPic.current.classList.toggle('product-otherImg');
+    secondPic.current.classList.toggle('product-mainImg');
+    secondPic.current.classList.toggle('product-otherImg');
+  };
+
   useEffect(() => {
-    // call api to get detail about product 
+    // call api to get detail about product
     requestAction({
       url: `http://54.164.43.47:3000/products/${match.params.productId}`,
       onSucess: saveProductDetail,
       label: 'isLoading',
     });
 
-    // call api to get all the size available 
-   /*  requestAction({
+    // call api to get all the size available
+    /*  requestAction({
       url: `http://54.164.43.47:3000/skus-by-product/${match.params.productId}`,
       onSucess: saveSizeAvailable,
       label: 'sizeLoading',
-    });  */   
+    });  */
   }, []);
 
   // Display the loading icon by default
@@ -44,8 +54,8 @@ export const Product = ({
       <>
         <Hierarchy match={match} categoryName={product.category.name} productName={product.name} />
         <section className="product">
-          <img className="product-mainImg" src={product.picture.picture1} alt="Produit" />
-          <img className="product-otherImg" src={product.picture.picture2} alt="Produit" />
+          <img className="product-mainImg" ref={mainPic} onClick={handleChangePic} src={product.picture.picture1} alt="Produit" />
+          <img className="product-otherImg" ref={secondPic} onClick={handleChangePic} src={product.picture.picture2} alt="Produit" />
           <div className="product-aside">
             <h2 className="product-name">{product.name}</h2>
             <p className="product-description">{product.description}</p>
