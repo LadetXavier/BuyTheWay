eb6fcc72078889d199fa6660e268b31bf86a937c
Tristan LE PARE <tristan.lepare@hotmail.fr>
2020-03-11 16:34:33 +0100

    presque fini

--- src/components/Header/category.js
+++ src/components/Header/category.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const Category = React.forwardRef(({ class1, class2, class3, data }, ref) => {  
+const Category = React.forwardRef(({ class1, class2, class3, data }, ref) => {
   let men,women = (<></>);
   if(!data.menLoading && !data.womenLoading) {
     console.log(data);
@@ -12,24 +12,26 @@ const Category = React.forwardRef(({ class1, class2, class3, data }, ref) => {
   return (
     <div className={`link-container ${class3}`} ref={ref}>
       <div className={class1}>
-        <li><Link id="men" className="categories" to="/">HOMMES</Link></li>
+        <div><Link id="men" className="categories" to="/">HOMMES</Link></div>
         <div className={class2}>
           {men}
         </div>
       </div>
       <div className={class1}>
-        <li><Link id="women" className="categories" to="/">FEMMES</Link></li>
+        <div><Link id="women" className="categories" to="/">FEMMES</Link></div>
         <div className={class2}>
           {women}
         </div>
       </div>
-      <div className={`${class1} link-container`}>
-        <li><Link id="trend" className="categories" to="/">TENDANCES</Link></li>
-        <li><Link id="challenges" className="categories" to="/">DEFIS</Link></li>
+      <div className={`${class1}`}>
+        <div><Link id="trend" className="categories" to="/">TENDANCES</Link></div>
+      </div>
+      <div className={`${class1}`}>
+        <div><Link id="challenges" className="categories" to="/">DEFIS</Link></div>
       </div>
     </div>
 );
-})
+});
 
 Category.propTypes = {
   class1: PropTypes.string,
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,11 +10,11 @@ import Icons from 'src/components/Header/icons';
 import 'src/assets/styles/header.scss';
 import Logo from 'src/assets/Pictures/logoBuyTheWay.png';
 
-import {loadNav} from 'src/actions/shop.js';
+import { loadNav } from 'src/actions/shop.js';
 import { apiError } from 'src/actions/apiActions.js';
 
 
-const Header = ({requestAction,nav,womenLoading,menLoading}) => {    
+const Header = ({requestAction,nav,womenLoading,menLoading}) => {
   useEffect(() => {
     requestAction({
       url: `http://54.164.43.47:3000/gender/homme`,
@@ -25,10 +25,10 @@ const Header = ({requestAction,nav,womenLoading,menLoading}) => {
 
     requestAction({
       url: `http://54.164.43.47:3000/gender/femme`,
-      onSuccess: (data) => ( loadNav(data,'femme')),
+      onSuccess: (data) => (loadNav(data, 'femme')),
       label: 'womenLoading',
     });
-  },[]);
+  }, []);
   const ref = React.createRef();
   const handleMobileClick = () => {
     ref.current.classList.toggle('displayNone');
@@ -37,12 +37,13 @@ const Header = ({requestAction,nav,womenLoading,menLoading}) => {
     <>
       <MediaQuery maxDeviceWidth={1199}>
         <nav className="navbar-mobile">
-          <div type="button" className="far fa-bars icons dropleft" onClick={handleMobileClick} />
-          <Link to="/"> <img src={Logo} alt="logo" className="logo-mobile" /> </Link>
-          <ul className="">
-            <Category ref={ref} class1="dropleft-content" class3="displayNone" data={{ nav, womenLoading, menLoading }} />
-          </ul>
-          <Icons />
+          <div className="dropleft">
+            <div className="far fa-bars icons" onClick={handleMobileClick} />
+            <Link to="/"> <img src={Logo} alt="logo" className="logo-mobile" /> </Link>
+            <Icons />
+            <Category ref={ref} class1="" class3="displayNone" data={{ nav, womenLoading, menLoading }} />
+          </div>
+
         </nav>
       </MediaQuery>
 
@@ -52,10 +53,10 @@ const Header = ({requestAction,nav,womenLoading,menLoading}) => {
           <Animate component="div" className="nav-links" appear="fadeInLeft" durationAppear={1750}>
             <Link to="/"> <img src={Logo} alt="logo" className="logo-desktop" /> </Link>
           </Animate>
-          <Animate component="ul" className="nav-links" appear="fadeInDown" durationAppear={1750}>
-            <Category ref={ref} class1="dropdown" class2="dropdown-content" data={{nav,womenLoading,menLoading}} />
+          <Animate component="div" className="nav-links" appear="fadeInDown" durationAppear={1750}>
+            <Category ref={ref} class1="dropdown" class2="dropdown-content" data={{ nav, womenLoading, menLoading }} />
           </Animate>
-          <Animate component="ul" className="nav-links" appear="fadeInRight" durationAppear={1750}>
+          <Animate component="div" className="nav-links" appear="fadeInRight" durationAppear={1750}>
             <Icons />
           </Animate>
         </nav>
