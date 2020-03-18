49a9425e3d7f9ba3916505852e76f44210c68575
Tristan LE PARE <tristan.lepare@hotmail.fr>
2020-03-18 12:14:50 +0100

    preview video done

--- src/pages/home.js
+++ src/pages/home.js
@@ -46,8 +46,10 @@ export const Home = ({
     displayed = (
       <>
         <div className="index">
-          <div id="introVideo">
-            {/*<iframe width="1335" height="405" src="https://www.youtube.com/embed/8lqrok_ECFM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="true" />*/}
+          <div class="video-container" >
+            <div class="video-foreground">
+              <iframe width="1335" height="405" src="https://www.youtube.com/embed/8lqrok_ECFM?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=8lqrok_ECFM&mute=1" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen="true" />
+            </div>
           </div>
           <div id="tendances">
             <p>Tendances</p>
