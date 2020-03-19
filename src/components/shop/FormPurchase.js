6ca0ae79a39511ff93561e2c41b684fb2efd4abc
LadetXavier <xavier.ladet@yahoo.fr>
2020-03-19 10:10:31 +0100

    fix handling stock

--- src/components/shop/FormPurchase.js
+++ src/components/shop/FormPurchase.js
@@ -2,29 +2,37 @@ import React, {useEffect,useState} from 'react'
 import Cookies from 'js-cookie';
 import Modal from 'src/components/shop/Modal.js';
 
-
+// Component to handle adding product to cart
 const FormPurchase = ({sizeAvailable,requestAction, item}) => {  
   let displayed = <> </>;
-  const [sizeSelected, setSizeSelected] = useState("");
+  //console.log(sizeAvailable);
+
+  const [sizeSelected, setSizeSelected] = useState(""); 
   const [quantitySelected, setQuantitySelected] = useState(1);
-  let [modalOn,setModalOn] = useState(false);  
-  let [modal,setModal] = useState(<></>);  
+  let [modalOn,setModalOn] = useState(false); // switch to activate modal 
+  let [modal,setModal] = useState(<></>); // state to handle dynamic modal 
   let [stock,setstock] = useState(0);  
   
-  const getStock = () => {
+  //Function used to calculate stock available of the selected size
+  const getStock = (sizeWanted) => {
     let stockAvailable = sizeAvailable.find((size) => {
-      return size.size === sizeSelected;
+      return size.size === sizeWanted;
     })
     stockAvailable = stockAvailable.quantity;
     return (stockAvailable);
   }
 
+  // Handler size change
   const handleSizeChange = (e) => {
     setSizeSelected(e.target.value);
-    let newStock = getStock();
+    let newStock = getStock(e.target.value);
     setstock(newStock); 
+    // when we switch size, test if selected quantity is > to available stock
     if( quantitySelected > newStock) {
       setQuantitySelected(newStock);
+    }
+    else if (quantitySelected <= 0) {
+      setQuantitySelected(1);
     }
   }
   const handleQuantityChange = (e) => {
@@ -33,6 +41,7 @@ const FormPurchase = ({sizeAvailable,requestAction, item}) => {
   const handleSubmit = (e) => {    
     e.preventDefault();
     setModalOn(true);
+    // If user connected then send a axios request, else error message
     if(Cookies.get('user_id') !== undefined) { 
       setModal(<Modal text="L'article a été ajouté au panier" closer={setModalOn} timeout = "3000"/>)     
       requestAction({
@@ -57,13 +66,14 @@ const FormPurchase = ({sizeAvailable,requestAction, item}) => {
       setModal(<Modal text="L'achat de produit est réservé aux utilisateurs connectés" closer={setModalOn} timeout = "3000"/>);
     }
   }
-
+  // if axios request worked fine and it's the first time then we initialise state
   if(sizeAvailable !== undefined) {
     if(sizeSelected === "") {
       setSizeSelected(sizeAvailable[0].size);
       setstock(sizeAvailable[0].quantity);      
     }
     let quantity = null;
+    // handling stock
     if(stock > 0) {      
       quantity= (
       <>
